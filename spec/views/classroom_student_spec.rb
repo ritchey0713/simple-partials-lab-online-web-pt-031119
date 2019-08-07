57b88606ff9f9dd263da377795490ec4dad09e30
David Ritchey <ritchey0713@gmail.com>
2019-08-07 18:31:53 +0000

    Automatically backed up by Learn

--- spec/views/classroom_student_spec.rb
+++ spec/views/classroom_student_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe "classroom_show_view" do
   it "renders classroom information on the show view" do
     view.lookup_context.prefixes = %w[students]
-    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
     assign(:student, student)
     render :template => "students/show.html.erb"
     expect(rendered).to match /Bobby/
