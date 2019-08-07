72f8dc1480968182332ec700d34bcb7988e27dca
David Ritchey <ritchey0713@gmail.com>
2019-08-07 18:32:06 +0000

    Automatically backed up by Learn

--- spec/views/classroom_student_spec.rb
+++ spec/views/classroom_student_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe "classroom_show_view" do
 
   it "renders a students/student partial" do
     view.lookup_context.prefixes = %w[students]
-    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
     assign(:student, student)
     render :template => "students/show.html.erb"
     expect(rendered).to render_template(:partial => "students/_student")
   end
 
   it "displays the student information from the partial" do
-    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(25.years.ago, 18.years.ago))
+    student = Student.create(name: 'Bobby', hometown: Faker::Address.city, birthday: Faker::Date.between(from: 25.years.ago, to: 18.years.ago))
     assign(:student, student)
     render :partial => "students/student.html.erb"
     expect(rendered).to match /Bobby/
